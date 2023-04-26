f42343aa4cc569145067df70ba374db8cf8b92bb
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-04-26 02:08:15 +0000

    Added public and secret keys

--- src/core/config.py
+++ src/core/config.py
@@ -3,9 +3,17 @@
 # Third party imports
 import logging
 
+from starlette.config import Config
+
+config = Config(".env")
+
+
 log = logging.getLogger(__name__)
 
 
-PROJECT_NAME = "credit-api"
+PROJECT_NAME = "paystack-demo"
 VERSION = "1.0"
 API_PREFIX = "/api"
+
+PAYSTACK_PUBLIC_KEY = config("PAYSTACK_PUBLIC_KEY")
+PAYSTACK_SECRET_KEY = config("PAYSTACK_SECRET_KEY")
